0c17584a480fa079588f2904d27eab0fc776e9a9
Chasith-Randima <chasith19@gmail.com>
2021-07-11 11:30:02 +0530

    created sign up and login functions

--- constrollers/authController.js
+++ constrollers/authController.js
@@ -265,7 +265,7 @@ exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError('you do not have permission to perform this action', 403)
+        new AppError('you do not have permission to perform this action ', 403)
       );
     }
 
