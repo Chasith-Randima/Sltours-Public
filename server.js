290664ee977f47e164a0283664dc2241bf722e4e
Chasith-Randima <chasith19@gmail.com>
2021-08-16 16:19:42 +0530

    Updated the pakage.json and server.js files

--- server.js
+++ server.js
@@ -1,47 +1,54 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-process.on("uncaughtException",err => {
-    
-    console.log("UNCAUGHT EXCEPTION.........SHUTTING DOWN");
-    console.log(err.name, err.message,err);
-    process.exit(1);
-    
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION.........SHUTTING DOWN');
+  console.log(err.name, err.message, err);
+  process.exit(1);
 });
 
+dotenv.config({ path: './config.env' });
 
-dotenv.config({path:"./config.env"});
+const app = require('./app');
 
-const app = require("./app");
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    //useFindModify:false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection succesfull');
+  });
 
 //console.log(process.env);
 
-mongoose.connect(process.env.DATABASE_LOCAL,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
-    //useFindModify:false,
-    useUnifiedTopology: true 
+// mongoose.connect(process.env.DATABASE_LOCAL,{
+//     useNewUrlParser:true,
+//     useCreateIndex:true,
+//     //useFindModify:false,
+//     useUnifiedTopology: true
 
-}).then(() => {
-    
-    console.log("DB connection succesfull");
-});
+// }).then(() => {
+
+//     console.log("DB connection succesfull");
+// });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port , () => {
-    console.log(`server listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
 
-
-process.on("unhadledRejection",err => {
-    
-    console.log("UNHADLED REJECTION.........SHUTTING DOWN");
-    console.log(err.name, err.message);
-    server.close(()=>{
-        process.exit(1);
-    });
-
+process.on('unhadledRejection', (err) => {
+  console.log('UNHADLED REJECTION.........SHUTTING DOWN');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
-
-
