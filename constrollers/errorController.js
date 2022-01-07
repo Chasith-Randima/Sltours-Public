b279b253f60cbe582f86256ee15b8c5f8fd5c808
Chasith-Randima <chasith19@gmail.com>
2022-01-07 23:47:38 +0530

    trying to fix catchAsync error

--- constrollers/errorController.js
+++ constrollers/errorController.js
@@ -1,115 +1,103 @@
-const AppError = require("../utils/appError");
-const factory = require("./handlerFactory");
-const Tour = require("../models/tourModel"); 
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
 
-const handleCastErrorDB = err => {
-    const message = `Invalid ${err.path}:${err.value}`;
-    return new AppError(message,400);
-};
+const handleCastErrorDB = catchAsync((err) => {
+  const message = `Invalid ${err.path}:${err.value}`;
+  return new AppError(message, 400);
+});
 
-const handleDuplicateFieldsDB = err => {
-    const value = err.errmsg.match(/([""])(\\?.)*?\1/)[0];
-    console.log(value);
+const handleDuplicateFieldsDB = catchAsync((err) => {
+  const value = err.errmsg.match(/([""])(\\?.)*?\1/)[0];
+  console.log(value);
 
-    const message = `Duplicate field value:${value}. Please use another value `
-    
-    return new AppError(message,400);
-};
+  const message = `Duplicate field value:${value}. Please use another value `;
 
-const handleValidationErrorDB = err => {
-    const errors = Object.values(err.errors).map(el => el.message);
+  return new AppError(message, 400);
+});
 
+const handleValidationErrorDB = catchAsync((err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
 
+  const message = `Invalid inputdata.${errors.join('. ')}`;
+  return new AppError(message, 400);
+});
 
-    const message = `Invalid inputdata.${errors.join(". ")}`;
-    return new AppError(message,400);
+const handleJWTError = catchAsync(
+  () => new AppError('Invalid token , please log in again', 401)
+);
 
-};
+const handleJWTExpiredError = catchAsync(
+  () => new AppError('your token has expired,please log in again', 401)
+);
 
-const handleJWTError = () => new AppError("Invalid token , please log in again",401);
+const sendErrorDev = catchAsync((err, req, res) => {
+  console.log(req.originalUrl);
 
-const handleJWTExpiredError = () => new AppError("your token has expired,please log in again",401);
-
-const sendErrorDev = (err,req,res)=>{
-    console.log(req.originalUrl);
-
-    if(req.originalUrl.startsWith("/api")){
-
+  if (req.originalUrl.startsWith('/api')) {
     return res.status(err.statusCode).json({
-        status:err.status,
-        error:err,
-        message:err.message,
-        stack:err.stack
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
     });
+  }
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went Wrong...!',
+    msg: err.message,
+  });
+});
+
+const sendErrorProd = catchAsync((err, req, res) => {
+  if (req.originalUrl.startsWith('/api')) {
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
     }
-        return res.status(err.statusCode).render("error",{
-            title:"Something went Wrong...!",
-            msg:err.message
-        });
-    
-};
 
-const sendErrorProd = (err,req,res) =>{
-
-    if(req.originalUrl.startsWith("/api")){
-    if(err.isOperational){
-    return res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    });
-
-}
-
-    console.error("ERROR",err);
+    console.error('ERROR', err);
     return res.status(500).json({
-        staus:"error",
-        message:"something went very wrong!"
+      staus: 'error',
+      message: 'something went very wrong!',
     });
+  }
 
-}
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
 
-    if(err.isOperational){
-        return res.status(err.statusCode).json({
-            status:err.status,
-            message:err.message
-        });
-    
-    }
-    
-        console.error("ERROR",err);
-        return res.status(500).json({
-            staus:"error",
-            message:"Please try again later"
-        });
-    
+  console.error('ERROR', err);
+  return res.status(500).json({
+    staus: 'error',
+    message: 'Please try again later',
+  });
+});
 
+module.exports = catchAsync((err, req, res, next) => {
+  //console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-};
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    error.message = err.message;
 
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'validationError')
+      error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (error.name === 'TokenExpiredError')
+      error = handleJWTExpiredError(error);
 
-module.exports = (err,req,res,next) => {
-
-    //console.log(err.stack);
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || "error";
-
-    if(process.env.NODE_ENV ==="development"){
-        sendErrorDev(err,req,res);
-
-    }else if(process.env.NODE_ENV === "production"){
-        let error = {...err};
-        error.message = err.message;
-
-        if(error.name ==="CastError") error = handleCastErrorDB(error);
-        if(error.code === 11000) error = handleDuplicateFieldsDB(error);
-        if(error.name === "validationError") error = handleValidationErrorDB(error);
-        if(error.name === "JsonWebTokenError") error = handleJWTError(error);
-        if(error.name === "TokenExpiredError") error = handleJWTExpiredError(error);
-        
-        
-        sendErrorProd(error,req,res);
-    }
-
-    
-
-};
+    sendErrorProd(error, req, res);
+  }
+});
--- constrollers/userController.js
+++ constrollers/userController.js
@@ -1,10 +1,9 @@
-const User = require("./../models/userModel");
-const multer = require("multer");
-const sharp = require("sharp");
-const catchAsync = require("./../utils/catchAsync");
-const AppError = require("./../utils/appError");
-const factory = require("./handlerFactory");
-
+const User = require('./../models/userModel');
+const multer = require('multer');
+const sharp = require('sharp');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+const factory = require('./handlerFactory');
 
 // const multerStorage = multer.diskStorage({
 //     destination:(req,file,cb) => {
@@ -18,89 +17,92 @@ const factory = require("./handlerFactory");
 
 const multerStorage = multer.memoryStorage();
 
-const multerFilter = (req,file,cb) => {
-    if(file.mimetype.startsWith("image")){
-        cb(null,true);
-    }else{
-        cb(new AppError("Not an Image please upload only an image",400),false);
-    }
-};
-
+const multerFilter = catchAsync((req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an Image please upload only an image', 400), false);
+  }
+});
 
 const upload = multer({
-    storage:multerStorage,
-    fileFilter:multerFilter
+  storage: multerStorage,
+  fileFilter: multerFilter,
 });
 
-exports.uploadUserPhoto = upload.single("photo");
+exports.uploadUserPhoto = catchAsync(upload.single('photo'));
 
-exports.resizeUserPhoto = catchAsync(async(req,res,next) => {
-    if(!req.file) return next();
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
 
-    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-    await sharp(req.file.buffer).resize(500,500).toFormat("jpeg").jpeg({ quality:90 }).toFile(`public/img/users/${req.file.filename}`);
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
 
-
-    next();
+  next();
 });
 
+const filterObj = catchAsync((obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
 
-const filterObj = (obj , ...allowedFields) => {
-    const newObj = {};
-    Object.keys(obj).forEach(el =>{
-        if(allowedFields.includes(el)) newObj[el] = obj[el];
-    });
-
-    return newObj;
-};
-
-exports.getMe = (req,res,next) => {
-    req.params.id = req.user.id;
-    next();
-};
-
-exports.updateMe = catchAsync(async(req,res,next) => {
-
-    if(req.body.password || req.body.passwordConfirm){
-        return next(new AppError("Thid rout is not for password update please use /updateMyPassword.",400));
-    };
-     
-    const filteredBody = filterObj(req.body,"name","email");
-    if(req.file) filteredBody.photo = req.file.filename;
-
-    const updatedUser = await User.findByIdAndUpdate(req.user.id,filteredBody,{
-        new:true,
-        runValidators:true
-    });
-
-    res.status(200).json({
-        status:"success",
-        data:{
-            user:updatedUser
-        }
-    });
+  return newObj;
 });
 
-
-exports.deleteMe = catchAsync(async(req,res,next) => {
-    await User.findByIdAndUpdate(req.user.id,{active:false});
-
-    res.status(204).json({
-        status:"success",
-        data:null
-    })
+exports.getMe = catchAsync((req, res, next) => {
+  req.params.id = req.user.id;
+  next();
 });
 
-exports.createUser = (req,res) => {
-    res.status(500).json({
-        status:"err",
-        message:"This route is not defiend,use sign up instead"
-    });
-};
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'Thid rout is not for password update please use /updateMyPassword.',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+exports.createUser = catchAsync((req, res) => {
+  res.status(500).json({
+    status: 'err',
+    message: 'This route is not defiend,use sign up instead',
+  });
+});
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
-
