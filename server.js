6e7825a137128d54daa4a69b0bc7581472c71309
Chasith-Randima <chasith19@gmail.com>
2022-01-07 23:23:52 +0530

    checking whether bug fixed

--- server.js
+++ server.js
@@ -1,47 +1,42 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-process.on("uncaughtException",err => {
-    
-    console.log("UNCAUGHT EXCEPTION.........SHUTTING DOWN");
-    console.log(err.name, err.message,err);
-    process.exit(1);
-    
-});
+// process.on("uncaughtException",err => {
 
+//     console.log("UNCAUGHT EXCEPTION.........SHUTTING DOWN");
+//     console.log(err.name, err.message,err);
+//     process.exit(1);
 
-dotenv.config({path:"./config.env"});
+// });
 
-const app = require("./app");
+dotenv.config({ path: './config.env' });
 
+const app = require('./app');
 
 //console.log(process.env);
 
-mongoose.connect(process.env.DATABASE_LOCAL,{
-    useNewUrlParser:true,
-    useCreateIndex:true,
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
     //useFindModify:false,
-    useUnifiedTopology: true 
-
-}).then(() => {
-    
-    console.log("DB connection succesfull");
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection succesfull');
+  });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port , () => {
-    console.log(`server listening on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
 
+// process.on("unhadledRejection",err => {
 
-process.on("unhadledRejection",err => {
-    
-    console.log("UNHADLED REJECTION.........SHUTTING DOWN");
-    console.log(err.name, err.message);
-    server.close(()=>{
-        process.exit(1);
-    });
+//     console.log("UNHADLED REJECTION.........SHUTTING DOWN");
+//     console.log(err.name, err.message);
+//     server.close(()=>{
+//         process.exit(1);
+//     });
 
-});
-
-
+// });
